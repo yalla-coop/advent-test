79fd6e7d12237f4486dd715bdbb5b8ee8dca0850
thejoefriel <joseph.s.friel@gmail.com>
2020-11-18 14:59:26 +0000

    Add condition for date

--- advent/src/App.js
+++ advent/src/App.js
@@ -1,35 +1,55 @@
-import slide1 from "./assets/slide-1.jpg";
-import slide2 from "./assets/slide-2.jpg";
 import "./App.css";
+import React from "react";
+import moment from "moment";
+import styled from "styled-components";
+
+const StyledLink = styled.a`
+  transition: all ease 0.2s;
+
+  ${(props) =>
+    props.open
+      ? `
+      :hover {
+        -webkit-animation: glow 1s ease-in-out infinite alternate;
+        -moz-animation: glow 1s ease-in-out infinite alternate;
+        animation: glow 1s ease-in-out infinite alternate;
+        color: darkred;
+  }`
+      : "cursor: default;"}
+`;
 
 function App() {
+  const open = (date) => {
+    const todayOrPast = moment(`11/${date}/2020`).diff(moment(), "days") <= 0;
+    return todayOrPast;
+  };
   return (
     <div className="App">
       <header className="App-header">
         <div className="Panel-1">
-          <a
+          <StyledLink
             id="Door-1"
-            className="Door"
-            href="https://www.thegingerbreadcity.com/1"
+            href={open(1) ? "https://www.thegingerbreadcity.com/1" : null}
+            open={open(1)}
           >
-            <p>1</p>
-          </a>
-          <a
-            id="Door-16"
-            className="Door"
-            href="https://www.thegingerbreadcity.com/16"
+            1
+          </StyledLink>
+          <StyledLink
+            id="Door-22"
+            href={open(22) ? "https://www.thegingerbreadcity.com/16" : null}
+            open={open(22)}
           >
-            <p>16</p>
-          </a>
+            22
+          </StyledLink>
         </div>
         <div className="Panel-2">
-          <a
+          <StyledLink
             id="Door-7"
-            className="Door"
-            href="https://www.thegingerbreadcity.com/16"
+            href={open(7) ? "https://www.thegingerbreadcity.com/1" : null}
+            open={open(7)}
           >
-            <p>7</p>
-          </a>
+            7
+          </StyledLink>
         </div>
         <div className="Panel-3" />
       </header>
